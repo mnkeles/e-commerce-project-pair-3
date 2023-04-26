86fc28bdbb07e6d1ef90d208006ec29e3091cb60
Muhammed Necati Keleş <95759870+mnkeles@users.noreply.github.com>
2023-04-26 15:42:58 +0300

    Add files via upload

---
+++ ecommerce-pair3/src/main/java/com/etiya/ecommercepair3/business/concretes/CityManager.java
@@ -0,0 +1,10 @@
+package com.etiya.ecommercepair3.business.concretes;
+
+import com.etiya.ecommercepair3.business.abstracts.CityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CityManager implements CityService {
+}
---
+++ ecommerce-pair3/src/main/java/com/etiya/ecommercepair3/business/concretes/StreetManager.java
@@ -0,0 +1,10 @@
+package com.etiya.ecommercepair3.business.concretes;
+
+import com.etiya.ecommercepair3.business.abstracts.StreetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StreetManager implements StreetService {
+}
